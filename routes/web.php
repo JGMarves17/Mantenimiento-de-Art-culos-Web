29ec52bbf7005569b0b8a1b69078e30e6368aa71
JGMarves17 <gabrielmarves@hotmail.com>
2026-06-19 16:23:01 -0600

    fix rutas

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -12,13 +11,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Rutas del CRUD de Posts (Protegidas por login)
+// Rutas protegidas por login
 Route::middleware('auth')->group(function () {
     Route::resource('posts', PostController::class);
-    
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::view('profile', 'profile')->name('profile');
 });
 
 require __DIR__.'/auth.php';
